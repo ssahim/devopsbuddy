5bb525e4efcb34382e765fbc081dcb2b334c7b1e
Sayed Ahmad Sahim <sayedahmad.sahim@gmail.com>
2018-04-28 01:52:50 +0200

    initial

---
+++ src/main/java/com/devepsbuddy/exceptions/StripeException.java
@@ -0,0 +1,12 @@
+package com.devopsbuddy.exceptions;
+
+/**
+ * Created by tedonema on 13/05/2016.
+ */
+public class StripeException extends RuntimeException {
+
+    public StripeException(Throwable e) {
+        super(e);
+    }
+
+}
