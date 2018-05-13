7f6766628a5656b26e8a260c544ac790ae79e72d
Sayed Ahmad Sahim <sayedahmad.sahim@gmail.com>
2018-05-14 01:18:46 +0200

    added the i18n capabilities to the web skeliton

---
+++ src/main/java/com/devepsbuddy/config/I18Config.java
@@ -0,0 +1,17 @@
+package com.devepsbuddy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class I18Config {
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:i18n/messages");
+        messageSource.setCacheSeconds(1800); //reload messages every 10 seconds
+        return messageSource;
+    }
+}
