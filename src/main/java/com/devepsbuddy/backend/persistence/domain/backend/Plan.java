b726dca2e6ee179ec2e198ce8e329e09a3529061
Sayed Ahmad Sahim <sayedahmad.sahim@gmail.com>
2018-05-29 00:43:37 +0200

    created the jpa entity model for spring security

---
+++ src/main/java/com/devepsbuddy/backend/persistence/domain/backend/Plan.java
@@ -0,0 +1,54 @@
+package com.devepsbuddy.backend.persistence.domain.backend;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Plan implements Serializable {
+
+//    The serial version ID fro serializable classes
+    private static final long serialVersionUID=1L;
+
+//    Default Constructor
+
+    @Id
+    private int id;
+
+    private String name;
+
+    public Plan() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Plan plan = (Plan) o;
+        return id == plan.id;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/devepsbuddy/backend/persistence/domain/backend/UserRole.java
@@ -0,0 +1,56 @@
+package com.devepsbuddy.backend.persistence.domain.backend;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "user_role")
+public class UserRole implements Serializable {
+
+    private static final long serialVersionUID=1L;
+
+    public UserRole() {
+    }
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserRole userRole = (UserRole) o;
+        return Objects.equals(user, userRole.user) &&
+                Objects.equals(role, userRole.role);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(user, role);
+    }
+}
